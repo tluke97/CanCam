8591818606f576bdbf1086a9f4830c24d28ae33a
Tanner Luke <TannerLuke@macbook-pro.local>
2018-08-10 21:39:50 -0500

    Initial Commit

---
+++ CanCam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CanCam
+//
+//  Created by Tanner Luke on 8/10/18.
+//  Copyright © 2018 Tanner Luke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
